ac37d9c975ff78df41ca968961d2fd2a239d3fbb
Mhleli07 <mbonambimhleli@gmail.com>
2023-03-21 17:38:11 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
-* main -Entry point
-* prints  _putchar
-* Return: Always 0 (success)
-*/
+ * main - Entry Block
+  *Description: prints _putchar followed by a new line
+ * Return: 0
+ */
 int main(void)
 {
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-return (0);
+	char c[8] = "_putchar";
+
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i ;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (char = 'a'; char <= 'z'; n++)
+		{
+			_putchar(char);
+		}
+		ch++;
+		_putchar('\n');
+	}
+}
